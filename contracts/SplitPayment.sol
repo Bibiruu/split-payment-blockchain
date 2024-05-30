d12bffdba19ab7dc88d5edf05299ff301b30b9a7
Maria <so.mariagloria@gmail.com>
2024-05-31 00:32:41 +0300

    modifier added

--- contracts/SplitPayment.sol
+++ contracts/SplitPayment.sol
@@ -6,14 +6,24 @@ contract SplitPayment {
     address public owner;
 
     constructor(address _owner) {
+        //executed once when the contract is deployed
         owner = _owner;
     }
-
-    function send(address payable[] memory to, uint[] memory amount) public payable {
-        require(msg.sender == owner, "only owner can send transfers");
-        require(to.length == amount.length, "to and amount arrays must have length");
-        for(uint i = 0; i < to.length; i++)  {
+    //to, an array of payable(send and receive ETH) addresses (recipients)
+    function send(
+        address payable[] memory to,
+        uint[] memory amount
+    ) public payable onlyOwner {
+        require(
+            to.length == amount.length,
+            "to and amount arrays must have length"
+        );
+        for (uint i = 0; i < to.length; i++) {
             to[i].transfer(amount[i]);
         }
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner can send transfers");
+        _;
     }
 }
